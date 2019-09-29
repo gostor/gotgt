8be0578d4ce6c4719ddcbd308230dd027df9b1c6
Lei Xue <carmark.dlut@gmail.com>
2019-09-29 09:40:49 +0800

    update ceph-deploy version to v2.0.2

--- ci/ceph_install.sh
+++ ci/ceph_install.sh
@@ -6,19 +6,10 @@ set -x
 sudo apt-get install -y python-virtualenv
 
 # ceph-deploy and ceph
-
-WORKDIR=$HOME/workdir
 CEPH_RELEASE=jewel
-mkdir $WORKDIR
-pushd $WORKDIR
 
-git clone -b "v2.0.0" --single-branch --depth 1 https://github.com/ceph/ceph-deploy
-pushd ceph-deploy
-./bootstrap
-./ceph-deploy install --release ${CEPH_RELEASE} `hostname`
-./ceph-deploy pkg --install librados-dev `hostname`
-./ceph-deploy pkg --install librbd-dev `hostname`
-./ceph-deploy pkg --install libcephfs-dev `hostname`
-popd # ceph-deploy
-
-popd # workdir
+sudo pip install ceph-deploy
+ceph-deploy install --release ${CEPH_RELEASE} `hostname`
+ceph-deploy pkg --install librados-dev `hostname`
+ceph-deploy pkg --install librbd-dev `hostname`
+ceph-deploy pkg --install libcephfs-dev `hostname`
